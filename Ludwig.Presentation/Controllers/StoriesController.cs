5361a9d8f9717fad07438fd6ecffaa8232173440
Mani Moayedi <acidmanic.moayedi.com>
2022-11-12 21:37:03 +0330

    Add Crud for UserStory

---
+++ Ludwig.Presentation/Controllers/StoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnTier.Controllers;
+using Ludwig.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ludwig.Presentation.Controllers
+{
+    [ApiController]
+    [Route("stories")]
+    public class StoriesController : CrudControllerBase<UserStory,long>
+    {
+        
+    }
+}
