36b6c5fe49a392b1825f8c7dd42dccf159fc36c6
Mani Moayedi <acidmanic.moayedi.com>
2022-11-16 01:31:39 +0330

    Make colapsible collapse on losing focus

--- Ludwig.Ui/src/app/collapsible/collapsible.component.ts
+++ Ludwig.Ui/src/app/collapsible/collapsible.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'collapsible',
@@ -7,19 +7,38 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class CollapsibleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer:Renderer2) { }
 
   @Input('collapsed') collapsed:boolean=true;
   @Output('collapsedChange') collapsedChange:EventEmitter<boolean>=new EventEmitter<boolean>();
   @Input('alternative-text') alternativeText:string='...';
   @Input('collapse-button-classes') colButCss:string="";
 
+  @ViewChild('meView') meView:any;
+
   ngOnInit(): void {
+
+    this.renderer.listen('body','click',(e:Event) => {
+
+          var insideClick: boolean = false;
+
+          e.composedPath().forEach(f => insideClick = insideClick || f == this.meView.nativeElement);
+
+          if (!insideClick) {
+            this.collapse();
+          }
+    });
+
   }
 
 
   toggle(){
     this.collapsed=!this.collapsed;
+    this.collapsedChange.emit(this.collapsed);
+  }
+
+  collapse(){
+    this.collapsed=true;
     this.collapsedChange.emit(this.collapsed);
   }
 }
--- Ludwig.Ui/src/app/editable-user-story/editable-user-story.component.ts
+++ Ludwig.Ui/src/app/editable-user-story/editable-user-story.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, Renderer2} from '@angular/core';
 import {UserStoryModel} from "../models/user-story.model";
 import {StoryUserModel} from "../models/story-user.model";
 
