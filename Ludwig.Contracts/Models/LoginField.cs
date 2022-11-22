d3595660f6562a2f1db91593edf97e56b1c97970
Mani Moayedi <acidmanic.moayedi.com>
2022-11-22 13:52:35 +0330

    Update loginField fields!

--- Ludwig.Contracts/Models/LoginField.cs
+++ Ludwig.Contracts/Models/LoginField.cs
@@ -5,13 +5,34 @@ namespace Ludwig.Contracts.Models
         public string Name { get; set; }
 
         public bool UiProtectedValue { get; set; }
+        
+        
+        public string Description { get; set; }
+        
+        public string DisplayName { get; set; }
+        
 
-        public static LoginField Username { get; } = new LoginField { Name = "Username", UiProtectedValue = false };
+        public static LoginField Username { get; } = new LoginField
+        {
+            Name = "Username", UiProtectedValue = false,
+            DisplayName = "Username"
+        };
         
-        public static LoginField Password { get; } = new LoginField { Name = "Password", UiProtectedValue = true };
+        public static LoginField Password { get; } = new LoginField { 
+            Name = "Password",
+            UiProtectedValue = true ,
+            DisplayName = "Password"
+        };
         
-        public static LoginField Code { get; } = new LoginField { Name = "Code", UiProtectedValue = false };
+        public static LoginField Code { get; } = new LoginField {
+            Name = "Code",
+            UiProtectedValue = false ,
+            DisplayName = "Code"
+        };
         
-        public static LoginField Otp { get; } = new LoginField { Name = "Otp", UiProtectedValue = false };
+        public static LoginField Otp { get; } = new LoginField { Name = "Otp",
+            UiProtectedValue = false ,
+            DisplayName = "One Time Password."
+        };
     }
 }
