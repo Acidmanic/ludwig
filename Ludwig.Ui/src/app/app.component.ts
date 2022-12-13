12dfdf8f55f740aeacb0cbf64a571a658e04d246
Mani Moayedi <acidmanic.moayedi.com>
2022-12-13 05:08:23 +0330

    Fix issues caused by authorized api call in main component

--- Ludwig.Ui/src/app/app.component.ts
+++ Ludwig.Ui/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {PriorityModel} from "./models/priority-model";
 import {LoginManagerService} from "./services/login-manager/login-manager.service";
 import {ExportInfoModel} from "./models/export-info-model";
 import {ExportService} from "./services/export/export.service";
+import {NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -18,49 +19,48 @@ export class AppComponent implements OnInit,OnDestroy{
 
 
   constructor(public svcLogin:LoginManagerService,
-              private svcExports:ExportService) {
+              private svcExports:ExportService,
+              private router:Router) {
+
+    this.router.events.subscribe({
+      next: ev => {
+        if (ev instanceof NavigationStart) {
+
+          console.log('Route change detected');
+
+          this.updateExportersMenu();
+        }
+      }
+    });
+
+
   }
 
 
   ngOnInit(): void {
     console.log("Main Component Initialized");
 
-    this.svcExports.getAvailableExports().subscribe({
-      next: info => {
-        this.exportInfos=info;
-        console.log('got exporters',this.exportInfos);
-      },
-      error:err => {
-        console.log(err);
-      },
-      complete: () => {}
-    });
 
+
+
+  }
+
+  private updateExportersMenu(){
+    if(this.svcLogin.isLoggedIn){
+      this.svcExports.getAvailableExports().subscribe({
+        next: info => {
+          this.exportInfos=info;
+        },
+        error:err => {
+          console.log(err);
+        },
+        complete: () => {}
+      });
+    }
   }
 
   ngOnDestroy(): void {
 
   }
 
-
-
-  //
-  // usernameValue:string='Acidmanic';
-  // passwordValue:string='sphere';
-  //
-  // authorize(){
-  //
-  //   let url = 'http://litbid.ir:8888/rest/api/2/myself'
-  //   let usernamePassword = this.usernameValue+':'+this.passwordValue;
-  //   let token = btoa(usernamePassword);
-  //   let authorization = 'Basic ' + token;
-  //
-  //
-  //   this.http.get(url,{headers:{authorization:authorization}}).subscribe({
-  //     next: valu => console.log('Received as auth response',valu),
-  //     error: err => console.log('Received auth Error',err),
-  //     complete: () => {}
-  //   });
-  // }
-  //
 }
